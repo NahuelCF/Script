9aa2f4f4389ca145217384c496fb4b3e71d074e7
Nahuel <cristiannahuel95@gmail.com>
2021-03-10 12:11:54 -0300

    refactors

--- src/controller/script.js
+++ src/controller/script.js
@@ -2,29 +2,31 @@ const fetch = require("node-fetch")
 const { Item } = require("../domain")
 const readline = require("readline")
 
-let usersId = ""
-
 const getUsersId = () => {
     let interfazCaptura = readline.createInterface({ input: process.stdin, output: process.stdout })
     interfazCaptura.question("", function (res) {
         interfazCaptura.close()
-        seller(res)
+        getItems(res)
     })
 }
 
-const seller = async (users) => {
-    usersId = users.split(",")
+const getItems = async (users) => {
+    let usersId = users.split(",")
     let allItems = []
     usersId.map(async (userId) => {
         await fetch("https://api.mercadolibre.com/sites/MLA/search?seller_id=" + userId)
             .then(result => result.json())
             .then(items => allItems = items.results.map(item => Item.fromJSON(item)))
-        allItems.map(async (item) => {
-            await fetch("https://api.mercadolibre.com/categories/" + item.category_id)
-                .then(result => result.json())
-                .then(res => item.setCategoryName(res.name))
-            console.log(`${item.id}, ${item.title}, ${item.category_id}, ${item.category_name}`)
-        })
+        logItems(allItems)
+    })
+}
+
+const logItems = (allItems) => {
+    allItems.map(async (item) => {
+        await fetch("https://api.mercadolibre.com/categories/" + item.category_id)
+            .then(result => result.json())
+            .then(res => item.setCategoryName(res.name))
+        console.log(`${item.id}, ${item.title}, ${item.category_id}, ${item.category_name}`)
     })
 }
 
